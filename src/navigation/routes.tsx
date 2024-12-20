dc6e8405d3f7f968e1e9521b83b2fa69438f721d
BCIGRA <148233217+BCIGRA@users.noreply.github.com>
2024-12-21 01:17:14 +0700

    Update routes.tsx

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,9 +1,9 @@
 import type { ComponentType, JSX } from 'react';
 
-import { InitDataPage } from '@/pages/InitDataPage/InitDataPage';
 import { IndexPage } from '@/pages/IndexPage/IndexPage';
-import { LaunchParamsPage } from '@/pages/LaunchParamsPage/LaunchParamsPage';
-import { ThemeParamsPage } from '@/pages/ThemeParamsPage/ThemeParamsPage';
+import { InitDataPage } from '@/pages/InitDataPage/InitDataPage';
+import { LaunchParamsPage } from '@/pages/LaunchParamsPage/LaunchParamsPage.tsx';
+import { ThemeParamsPage } from '@/pages/ThemeParamsPage/ThemeParamsPage.tsx';
 import { TONConnectPage } from '@/pages/TONConnectPage/TONConnectPage';
 
 interface Route {
