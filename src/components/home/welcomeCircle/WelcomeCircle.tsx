cd877dd9343bea7ed1417c27487d15b69b1b4edc
BCIGRA <148233217+BCIGRA@users.noreply.github.com>
2024-12-21 17:00:22 +0700

    Update WelcomeCircle.tsx

--- src/components/home/welcomeCircle/WelcomeCircle.tsx
+++ src/components/home/welcomeCircle/WelcomeCircle.tsx
@@ -1,5 +1,3 @@
-// Mengimpor SCSS dari file relatif yang benar
-import '@assets/stylesheets/home/welcomeCircle.scss';
 import Badge from '@mui/material/Badge';
 import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
 import CloseIcon from '@mui/icons-material/Close';
@@ -9,9 +7,9 @@ const WelcomeCircle = ({ changeIcon, welcomeMessage, handleChangeIcon, image, wi
   let display, icon;
 
   if (changeIcon) {
-    icon = <CloseIcon style={{ fontSize: 40, color: "#5851DB" }} />
+    icon = <CloseIcon style={{ fontSize: 40, color: "#5851DB" }} />;
   } else {
-    icon = <QuestionAnswerOutlinedIcon style={{ fontSize: 40, color: "#5851DB" }} />
+    icon = <QuestionAnswerOutlinedIcon style={{ fontSize: 40, color: "#5851DB" }} />;
   }
 
   if (welcomeMessage) {
@@ -33,7 +31,21 @@ const WelcomeCircle = ({ changeIcon, welcomeMessage, handleChangeIcon, image, wi
     );
   }
 
-  return display;
+  return (
+    <>
+      {display}
+      <style jsx>{`
+        .avatar {
+          padding: 7px 7px;
+          border: 2px solid #000000;
+          border-radius: 50%; /* to get a circular shape */
+          cursor: pointer;
+          background-color: #000000;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default WelcomeCircle;
